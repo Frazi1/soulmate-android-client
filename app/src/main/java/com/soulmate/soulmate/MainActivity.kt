724115933e4273159505380950b63458597a918e
Fraz <diman896@gmail.com>
2018-03-10 21:36:40 +0300

    Test api on android.

--- app/src/main/java/com/soulmate/soulmate/MainActivity.kt
+++ app/src/main/java/com/soulmate/soulmate/MainActivity.kt
@@ -1,24 +1,46 @@
 package com.soulmate.soulmate
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
+import com.soulmate.dtos.UserAccountDto
 import com.soulmate.soulmate.api.TestApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
-    val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("http://localhost:8080")
+    private val retrofit: Retrofit = Retrofit.Builder()
+//            .baseUrl("http://localhost:8080")
+            .baseUrl("http://192.168.0.12:8080/")
             .addConverterFactory(JacksonConverterFactory.create())
             .build()
+    private val api = retrofit.create(TestApi::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btn = findViewById<Button>(R.id.my_button)
+        btn.setOnClickListener {
+            api.getAllUsers().enqueue(object : Callback<Iterable<UserAccountDto>> {
+                override fun onResponse(call: Call<Iterable<UserAccountDto>>?, response: Response<Iterable<UserAccountDto>>?) {
+                    Toast.makeText(
+                            applicationContext,
+                            response?.body()
+                                    ?.joinToString(" ") { it.firstName },
+                            Toast.LENGTH_LONG).show()
+                }
 
-        val api: TestApi = retrofit.create(TestApi::class.java)
-        val hello = api.getHelloWorldResponse();
+                override fun onFailure(call: Call<Iterable<UserAccountDto>>?, t: Throwable?) {
+                    Toast.makeText(applicationContext, t.toString(), Toast.LENGTH_LONG).show()
+                }
+            })
+        }
     }
+
 }
