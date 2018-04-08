7e7b1b356776c7f26e3bbccbfb5188332896b9f0
fraz <diman896@gmail.com>
2018-04-08 12:46:39 +0300

    Heroku server config

--- app/src/main/java/com/soulmate/soulmate/configuration/ConnectionPreferenceManager.kt
+++ app/src/main/java/com/soulmate/soulmate/configuration/ConnectionPreferenceManager.kt
@@ -9,10 +9,15 @@ import com.soulmate.soulmate.R
 class ConnectionPreferenceManager(context: Context, resources: Resources) : IConnectionPreferenceManager {
     private val preference = PreferenceManager.getDefaultSharedPreferences(context)
     private val keyServerUrl = resources.getString(R.string.key_devPref_serverUrl)
+    private val keyIsHerokuServer = resources.getString(R.string.key_devPref_isHerokuServer)
+
     private val defaultServerUrl = "http://192.168.0.100:8080"
+    private val herokuServerUrl = "https://cryptic-wildwood-80958.herokuapp.com"
 
     override var serverUrl: String
         get() {
+            if (preference.getBoolean(keyIsHerokuServer, false))
+                return herokuServerUrl
             if (!preference.contains(keyServerUrl))
                 serverUrl = defaultServerUrl
             return preference.getString(keyServerUrl, defaultServerUrl)
