6f25325bcb7fc8784e176c3ef58e3616c6faa4eb
Fraz <diman896@gmail.com>
2018-04-22 12:47:38 +0300

    Refactor error handler

--- app/src/main/java/com/soulmate/soulmate/api/errors/ToastErrorMessageHandler.kt
+++ app/src/main/java/com/soulmate/soulmate/api/errors/ToastErrorMessageHandler.kt
@@ -2,24 +2,14 @@ package com.soulmate.soulmate.api.errors
 
 import android.content.Context
 import android.widget.Toast
-import com.github.salomonbrys.kodein.Kodein
-import com.github.salomonbrys.kodein.instance
-import com.jakewharton.retrofit2.adapter.rxjava2.HttpException
 
 class ToastErrorMessageHandler(private val context: Context,
-                               private val errorMessageExtractor: IErrorMessageExtractor) : ErrorHandler() {
+                               private val errorMessageExtractor: IErrorMessageExtractor) : IErrorHandler {
 
 
     override fun handle(t: Throwable?) {
-        if(t is HttpException)
-            handle(t)
-        else
-            Toast.makeText(context, t.toString(), Toast.LENGTH_LONG)
-                    .show()
-    }
-
-    override fun handle(t: HttpException) {
-        Toast.makeText(context, errorMessageExtractor.errorMessage(t), Toast.LENGTH_LONG)
+        val message = if (t != null) errorMessageExtractor.errorMessage(t) else ""
+        Toast.makeText(context, message, Toast.LENGTH_SHORT)
                 .show()
     }
 }
