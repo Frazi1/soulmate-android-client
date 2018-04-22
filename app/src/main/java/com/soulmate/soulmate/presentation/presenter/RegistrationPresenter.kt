76c8c26ae8df74b958dec2874674e5a501776033
Fraz <diman896@gmail.com>
2018-04-22 12:47:58 +0300

    Fixed error when trying to register after logout

--- app/src/main/java/com/soulmate/soulmate/presentation/presenter/RegistrationPresenter.kt
+++ app/src/main/java/com/soulmate/soulmate/presentation/presenter/RegistrationPresenter.kt
@@ -4,6 +4,7 @@ import android.content.res.Resources
 import com.arellomobile.mvp.InjectViewState
 import com.github.salomonbrys.kodein.*
 import com.soulmate.soulmate.App
+import com.soulmate.soulmate.CredentialsStore
 import com.soulmate.soulmate.R
 import com.soulmate.soulmate.api.errors.IErrorHandler
 import com.soulmate.soulmate.presentation.view.RegistrationViewSoulmate
@@ -15,8 +16,10 @@ class RegistrationPresenter() : BaseSoulmatePresenter<RegistrationViewSoulmate>(
     private val resources: Resources by instance()
     private val authRepository: AuthRepository by instance()
     private val errorHandler: IErrorHandler by instance()
+    private val credentialsStore: CredentialsStore by instance()
 
     fun registerUser(email: String, password: String) {
+        credentialsStore.clear()
         authRepository.registerUser(email, password)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe ({
